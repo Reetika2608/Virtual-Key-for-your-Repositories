55065b7166bd7ca3c6c3031d184209cefd4a86ad
Yusuf Cinar <ycinar@cisco.com>
2018-03-25 00:38:03 +0000

    Fix test_xcommand
    This one is a weird one. Try on vcs box. It would need more correction there.

--- ni/test/managementconnector/test_xcommand.py
+++ ni/test/managementconnector/test_xcommand.py
@@ -7,8 +7,9 @@ import json
 
 from ni.managementconnector.config.managementconnectorproperties import ManagementConnectorProperties
 
-sys.path.append("/opt/c_mgmt/xcommand/")
-import c_mgmt
+# sys.path.append("/opt/c_mgmt/xcommand/")
+# import c_mgmt
+import ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand as c_mgmt
 
 class ErrorQueue():
     def __init__(self):
@@ -22,8 +23,9 @@ class CMgmtXCommandTest(unittest.TestCase):
     
     def setUp(self):
         # reload c_mgmt as xstatus and xcommand share the same module name
-        sys.path.insert(0, "/opt/c_mgmt/xcommand/")
-        reload(c_mgmt)
+        # sys.path.insert(0, "/opt/c_mgmt/xcommand/")
+        # reload(c_mgmt)
+        pass
 
     def test_usage_run_invalid_option(self):
         """Test running with invalid options"""
@@ -33,10 +35,10 @@ class CMgmtXCommandTest(unittest.TestCase):
         mock_callback.assert_called_with(expected_callback)
 
     @mock.patch('__builtin__.open')
-    @mock.patch('c_mgmt.U2C')
-    @mock.patch('c_mgmt.Http')
-    @mock.patch('c_mgmt.Config')
-    @mock.patch('c_mgmt.OAuth')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.U2C')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.Http')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.Config')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.OAuth')
     def test_mc_init(self, mock_oauth, mock_config, mock_http, mock_u2c, mock_open):
         """Test running init"""
         mock_callback = mock.MagicMock()
@@ -46,16 +48,16 @@ class CMgmtXCommandTest(unittest.TestCase):
         c_mgmt.run("init", cluster_id + " " + machine_account, None, mock_callback, None)
         mock_oauth.return_value.create_machine_account.assert_called_with('cluster_id',json.loads(machine_account))
 
-    @mock.patch('c_mgmt.Http')
-    @mock.patch('c_mgmt.Config')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.Http')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.Config')
     def test_mc_precheck(self, mock_config, mock_http):
         """Test running precheck"""
         mock_callback = mock.MagicMock()
         c_mgmt.run("precheck", 'x', None, mock_callback, None)
         mock_callback.assert_called_with('Found_good_certs')
 
-    @mock.patch('c_mgmt.time.sleep')
-    @mock.patch('c_mgmt.Config')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.time.sleep')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.Config')
     def test_mc_teardown(self, mock_config, mock_sleep):
         """Test running defuse"""
         mock_rest_cdb_adaptor = mock.MagicMock()
@@ -63,8 +65,8 @@ class CMgmtXCommandTest(unittest.TestCase):
         c_mgmt.run("defuse", 'abc 123', mock_rest_cdb_adaptor, mock_callback, None)
         mock_callback.assert_called_with('Defuse Complete')
 
-    @mock.patch('c_mgmt.time.sleep')
-    @mock.patch('c_mgmt.Config')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.time.sleep')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.Config')
     def test_mc_rollback(self, mock_config, mock_sleep):
         """Test running rollback"""
         mock_rest_cdb_adaptor = mock.MagicMock()
@@ -73,8 +75,8 @@ class CMgmtXCommandTest(unittest.TestCase):
         c_mgmt.run("rollback", 'c_cal', mock_rest_cdb_adaptor, mock_callback, error_queue)
         #mock_callback.assert_called_with('Rollback Complete')
 
-    @mock.patch('c_mgmt.time.sleep')
-    @mock.patch('c_mgmt.Config')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.time.sleep')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.Config')
     def test_mc_control(self, mock_config, mock_sleep):
         """Test running control command"""
         mock_rest_cdb_adaptor = mock.MagicMock()
@@ -84,11 +86,11 @@ class CMgmtXCommandTest(unittest.TestCase):
         mock_callback.assert_called_with('c_cal restart Complete')
 
     @mock.patch('os.system')
-    @mock.patch('c_mgmt.U2C')
-    @mock.patch('c_mgmt.Http')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.U2C')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.Http')
     @mock.patch('ni.managementconnector.platform.alarms.MCAlarm.clear_alarm')
     @mock.patch('ni.managementconnector.config.config.Config.write_blob')
-    @mock.patch('c_mgmt.OAuth')
+    @mock.patch('ni.files.opt.c_mgmt.xcommand.c_mgmt_xcommand.OAuth')
     def test_init_reregister(self, mock_oauth, mock_config_write_blob, mock_clear_alarm, mock_http, mock_u2c, mock_os_system):
         """Test running init reregister"""
         mock_callback = mock.MagicMock()
