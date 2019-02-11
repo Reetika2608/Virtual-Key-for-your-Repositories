454cd154cb6c5e21f4c02b9a8f2adfc4c01f1dbc
michkenn <michkenn@cisco.com>
2019-02-11 04:01:48 -0500

    SPARK-55085: U2C is invalidating federation URLs: remove CI urls from the U2C poller but populate the identity address if it is missing (#24)

--- src/managementconnector/cloud/u2c.py
+++ src/managementconnector/cloud/u2c.py
@@ -2,6 +2,7 @@
 import json
 from urlparse import urlsplit
 from managementconnector.cloud import schema
+from managementconnector.config.databasehandler import DatabaseHandler
 from managementconnector.config.managementconnectorproperties import ManagementConnectorProperties
 from managementconnector.platform.http import Http
 DEV_LOGGER = ManagementConnectorProperties.get_dev_logger()
@@ -17,8 +18,6 @@ class U2C(object):
                    "metrics": ManagementConnectorProperties.U2C_METRICS,
                    "remoteDispatcher": ManagementConnectorProperties.U2C_RD,
                    "feature": ManagementConnectorProperties.U2C_FEATURE,
-                   "idbroker": ManagementConnectorProperties.U2C_IDBROKER,
-                   "identity": ManagementConnectorProperties.U2C_IDENTITY,
                    "fms": ManagementConnectorProperties.U2C_FMS,
                    "atlasFusionAdminPortal": ManagementConnectorProperties.U2C_ADMIN_PORTAL}
 
@@ -26,6 +25,8 @@ class U2C(object):
         """ Constructor """
         self._config = config
         self._oauth = oauth
+        self._database_handler = DatabaseHandler()
+        self._identity_check = False
 
     def update_user_catalog(self):
         """ Update any service in the U2C User URL List"""
@@ -34,6 +35,10 @@ class U2C(object):
         user_url = self._config.read(ManagementConnectorProperties.U2C_USER_SERVICE_URL)
         service_catalogs = Http.get(host + user_url, headers=self._oauth.get_header(), schema=schema.U2C_SERVICES_RESPONSE)
         U2C.process_catalog(self._config, service_catalogs["services"])
+
+        if not self._identity_check:
+            self._check_identity_url(service_catalogs["services"])
+            self._identity_check = True
 
     @staticmethod
     def process_catalog(config, catalog):
@@ -50,3 +55,14 @@ class U2C(object):
                     url = parsed_url.scheme + "://" + parsed_url.netloc
 
                 config.write(U2C.service_map[service_entry["serviceName"]], {"value": json.dumps(url)})
+
+
+    def _check_identity_url(self, catalog):
+        """ Audit the identity URL directly in CDB. Populate it from U2C if it is missing """
+        identity_url = self._database_handler.read(ManagementConnectorProperties.U2C_IDENTITY_HOST)
+        if not identity_url or identity_url == "":
+            DEV_LOGGER.info('Detail="FMC_U2C populate_identity_url: getting identity URL from U2C"')
+            for service_entry in catalog:
+                if service_entry["serviceName"] == "identity":
+                    url = service_entry["logicalNames"][0]
+                    self._config.write(ManagementConnectorProperties.U2C_IDENTITY, {"value": json.dumps(url)})
