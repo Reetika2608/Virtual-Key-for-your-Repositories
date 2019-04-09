5df1233dbe8c3855d55981c223155ce1463266b1
cobarbou <cobarbou@cisco.com>
2019-04-09 11:24:35 +0100

    Change PR build format to 555.PR_NUM.Build_Num & Clean up extra pycs in files dir (#103)

--- setup_commands/clean_command.py
+++ setup_commands/clean_command.py
@@ -1,6 +1,5 @@
-from distutils.cmd import Command
-
 import os
+from distutils.cmd import Command
 
 
 class CleanCommand(Command):
@@ -21,6 +20,8 @@ class CleanCommand(Command):
             pyc="find ./src -name *.pyc | xargs rm -f; "
                 "find ./tests -name *.pyc | xargs rm -f; "
                 "find ./setup_commands -name *.pyc | xargs rm -f; ",
+            files_pyc="rm files/__init__.pyc files/opt/__init__.pyc files/opt/c_mgmt/__init__.pyc "
+                      "files/opt/c_mgmt/xcommand/__init__.pyc files/opt/c_mgmt/xstatus/__init__.pyc",
             build_dirs="rm -rf build/ dist/ debian/_build/ debian/data/ debian/control/_build ./*.egg-info test-results.xml",
         )
         for key, cmd in cmd_list.items():
