c35cb75ea93b5ec4f8792895d7ec9551900cfd86
Mads Rolsdorph <marolsdo@cisco.com>
2019-05-04 13:41:38 +0200

    Save source code when UI tests crash (#128)

--- tests_integration/ui_based_tests/basic_register_test.py
+++ tests_integration/ui_based_tests/basic_register_test.py
@@ -91,8 +91,12 @@ class BasicRegisterTest(unittest.TestCase):
         LOG.info("Running: tearDown")
         if sys.exc_info()[0]:
             class_name = self.__class__.__name__
-            LOG.info("Log directory: %s/%s_%s.png", self.log_directory, class_name, self._testMethodName)
+            LOG.info("Saving screenshot: %s/%s_%s.png", self.log_directory, class_name, self._testMethodName)
             self.web_driver.save_screenshot('%s/%s_%s.png' % (self.log_directory, class_name, self._testMethodName))
+
+            LOG.info("Saving source code: %s/%s_%s.txt", self.log_directory, class_name, self._testMethodName)
+            with open("%s/%s_%s.txt", "w") as f:
+                f.write(self.web_driver.page_source.encode('utf-8'))
         self.web_driver.quit()
 
     def test_connectors_can_be_enabled(self):
